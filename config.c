18e5caa7e392955b963495652b1f3b0a612ee80b
Bryan Li <bryanli1211@gmail.com>
2023-09-01 08:02:36 +0800

    add custom blocks

--- config.c
+++ config.c
@@ -4,7 +4,7 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
+    /*{"sb-mail",    600,  1 },
     {"sb-music",   0,    2 },
     {"sb-disk",    1800, 3 },
     {"sb-memory",  10,   4 },
@@ -13,7 +13,16 @@ Block blocks[] = {
     {"sb-record",  0,    7 },
     {"sb-volume",  0,    8 },
     {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+    {"sb-date",    1,    10},*/
+	{"sb-nettraf",	1,		0},
+	{"sb-cpu",		5,		2},
+	{"sb-memory",	5,		3},
+	{"sb-disk",		1800,	4},
+	{"sb-volume",	0,		5},
+	{"sb-battery",	5,		6},
+	{"sb-internet",	30,		7},
+	{"sb-clock",	1,		8},
+
 };
 
 const unsigned short blockCount = LEN(blocks);
